9344b0ab338d24edd649be289f82e83c7844b604
bromne <bromne@gmail.com>
2017-02-20 19:44:19 +0900

    unify indentation to 4-width spaces.

--- src/test/java/io/github/ikegamiyukino/neologdn/TestNeologdNormalizer.java
+++ src/test/java/io/github/ikegamiyukino/neologdn/TestNeologdNormalizer.java
@@ -7,36 +7,36 @@ import org.junit.Test;
 import io.github.ikegamiyukino.neologdn.NeologdNormalizer;
 
 public class TestNeologdNormalizer {
-  NeologdNormalizer normalizer = new NeologdNormalizer();
+    NeologdNormalizer normalizer = new NeologdNormalizer();
 
-  @Test
-  public void test() {
-    assertEquals(normalizer.normalize(" 餃 子 "), "餃子");
-    assertEquals(normalizer.normalize("０"), "0");
-    assertEquals(normalizer.normalize("ﾊﾝｶｸ"), "ハンカク");
-    assertEquals(normalizer.normalize("o₋o"), "o-o");
-    assertEquals(normalizer.normalize("majika━"), "majikaー");
-    assertEquals(normalizer.normalize("わ〰い"), "わい");
-    assertEquals(normalizer.normalize("スーパーーーー"), "スーパー");
-    assertEquals(normalizer.normalize("!#"), "!#");
-    assertEquals(normalizer.normalize("ゼンカク　スペース"), "ゼンカクスペース");
-    assertEquals(normalizer.normalize("お             お"), "おお");
-    assertEquals(normalizer.normalize("      おお"), "おお");
-    assertEquals(normalizer.normalize("おお      "), "おお");
-    assertEquals(normalizer.normalize("検索 エンジン 自作 入門 を 買い ました!!!"), "検索エンジン自作入門を買いました!!!");
-    assertEquals(normalizer.normalize("アルゴリズム C"), "アルゴリズムC");
-    assertEquals(normalizer.normalize("　　　ＰＲＭＬ　　副　読　本　　　"), "PRML副読本");
-    assertEquals(normalizer.normalize("Coding the Matrix"), "Coding the Matrix");
-    assertEquals(normalizer.normalize("南アルプスの　天然水　Ｓｐａｒｋｉｎｇ　Ｌｅｍｏｎ　レモン一絞り"),
-        "南アルプスの天然水Sparking Lemonレモン一絞り");
-    assertEquals(normalizer.normalize("南アルプスの　天然水-　Ｓｐａｒｋｉｎｇ*　Ｌｅｍｏｎ+　レモン一絞り"),
-        "南アルプスの天然水- Sparking*Lemon+レモン一絞り");
-    assertEquals(normalizer.normalize("ﾊﾟﾊﾟ"), "パパ");
-    assertEquals(normalizer.normalize("a˗֊‐‑‒–⁃⁻₋−"), "a-");
-    assertEquals(normalizer.normalize("あ﹣－ｰ—―─━ー"), "あー");
-    assertEquals(normalizer.normalize("チルダ~∼∾〜〰～"), "チルダ");
-    assertEquals(normalizer.normalize("(ﾟ∀ﾟ )"), "(゜∀゜)");
+    @Test
+    public void test() {
+        assertEquals(normalizer.normalize(" 餃 子 "), "餃子");
+        assertEquals(normalizer.normalize("０"), "0");
+        assertEquals(normalizer.normalize("ﾊﾝｶｸ"), "ハンカク");
+        assertEquals(normalizer.normalize("o₋o"), "o-o");
+        assertEquals(normalizer.normalize("majika━"), "majikaー");
+        assertEquals(normalizer.normalize("わ〰い"), "わい");
+        assertEquals(normalizer.normalize("スーパーーーー"), "スーパー");
+        assertEquals(normalizer.normalize("!#"), "!#");
+        assertEquals(normalizer.normalize("ゼンカク　スペース"), "ゼンカクスペース");
+        assertEquals(normalizer.normalize("お             お"), "おお");
+        assertEquals(normalizer.normalize("      おお"), "おお");
+        assertEquals(normalizer.normalize("おお      "), "おお");
+        assertEquals(normalizer.normalize("検索 エンジン 自作 入門 を 買い ました!!!"), "検索エンジン自作入門を買いました!!!");
+        assertEquals(normalizer.normalize("アルゴリズム C"), "アルゴリズムC");
+        assertEquals(normalizer.normalize("　　　ＰＲＭＬ　　副　読　本　　　"), "PRML副読本");
+        assertEquals(normalizer.normalize("Coding the Matrix"), "Coding the Matrix");
+        assertEquals(normalizer.normalize("南アルプスの　天然水　Ｓｐａｒｋｉｎｇ　Ｌｅｍｏｎ　レモン一絞り"),
+                "南アルプスの天然水Sparking Lemonレモン一絞り");
+        assertEquals(normalizer.normalize("南アルプスの　天然水-　Ｓｐａｒｋｉｎｇ*　Ｌｅｍｏｎ+　レモン一絞り"),
+                "南アルプスの天然水- Sparking*Lemon+レモン一絞り");
+        assertEquals(normalizer.normalize("ﾊﾟﾊﾟ"), "パパ");
+        assertEquals(normalizer.normalize("a˗֊‐‑‒–⁃⁻₋−"), "a-");
+        assertEquals(normalizer.normalize("あ﹣－ｰ—―─━ー"), "あー");
+        assertEquals(normalizer.normalize("チルダ~∼∾〜〰～"), "チルダ");
+        assertEquals(normalizer.normalize("(ﾟ∀ﾟ )"), "(゜∀゜)");
 
-  }
+    }
 
 }
